00e32bc7376d8e84709f6592fb3b2ef73251229b
Monica Nimmagadda <mnimmagadda18@amherst.edu>
2018-06-28 22:40:31 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -23,7 +23,7 @@ function logWhisper(string){
 function sayHiToGrandma(string){
   if (string==='hello')
     return "I can\'t hear you!"
-   if (string==='HELLO'
+   if (string==='HELLO')
     return "YES INDEED!"
    if (string==='I love you, Grandma.')
     return "I love you, too. "
